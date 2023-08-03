95d026e8795c8c6710247227e175a4e68f977caa
ClaytonEduard <clayton_eduard@hotmail.com>
2023-08-03 16:57:33 -0300

    Finalizando parte inicial, criando slicer de introducao Closes #1 Addresses #3

--- app/src/main/java/com/claytoneduard/beautysalon/MainActivity.java
+++ app/src/main/java/com/claytoneduard/beautysalon/MainActivity.java
@@ -4,11 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        //ocultando os botoes
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+        int cor = R.color.base;
+        addSlide(new FragmentSlide.Builder()
+                .background(cor)
+                .fragment(R.layout.intro_1)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(cor)
+                .fragment(R.layout.intro_2)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(cor)
+                .fragment(R.layout.intro_3)
+                .build());
+        addSlide(new FragmentSlide.Builder()
+                .background(cor)
+                .fragment(R.layout.intro_4)
+                .canGoForward(true)
+                .build());
     }
 }
