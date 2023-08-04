9426dae322d87fac7bcfd77233f1c466aa6fd005
ClaytonEduard <clayton_eduard@hotmail.com>
2023-08-04 17:04:34 -0300

    Criando verificação de campos e autenticação firebase login Implements #6 Implements #7 Implements #8

--- app/src/main/java/com/claytoneduard/beautysalon/config/ConfiguracaoFirebase.java
+++ app/src/main/java/com/claytoneduard/beautysalon/config/ConfiguracaoFirebase.java
@@ -7,10 +7,13 @@
 package com.claytoneduard.beautysalon.config;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ConfiguracaoFirebase {
 
     private static FirebaseAuth autenticacao;
+    private static DatabaseReference firebase;
 
     // retorna a instancia do FirebaseAth
     public static FirebaseAuth getFirebaseAutenticacao() {
@@ -19,5 +22,13 @@ public class ConfiguracaoFirebase {
             autenticacao = FirebaseAuth.getInstance();
         }
         return autenticacao;
+    }
+
+    // retorna a instancia do FirebaseDatabase
+    public static DatabaseReference getFirebaseDatabase() {
+        if (firebase == null) {
+            firebase = FirebaseDatabase.getInstance().getReference();
+        }
+        return firebase;
     }
 }
